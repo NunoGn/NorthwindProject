5e9c39cc6fa4877566906a52dbd71ecf43a3ce68
Nuno Correia <nunogncorreia25@gmail.com>
2020-08-20 14:10:42 +0100

    Criado windows form para gerir crud

---
+++ Northwind/NorthwindWin/FormShippers.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace NorthwindWin
+{
+    partial class FormShippers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gridShippers = new System.Windows.Forms.DataGridView();
+            this.lblCompanyName = new System.Windows.Forms.Label();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.txtCompanyName = new System.Windows.Forms.TextBox();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.btInsert = new System.Windows.Forms.Button();
+            this.btUpdate = new System.Windows.Forms.Button();
+            this.btDelete = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gridShippers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridShippers
+            // 
+            this.gridShippers.AllowUserToAddRows = false;
+            this.gridShippers.AllowUserToDeleteRows = false;
+            this.gridShippers.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridShippers.Location = new System.Drawing.Point(12, 12);
+            this.gridShippers.Name = "gridShippers";
+            this.gridShippers.ReadOnly = true;
+            this.gridShippers.RowHeadersVisible = false;
+            this.gridShippers.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gridShippers.Size = new System.Drawing.Size(322, 150);
+            this.gridShippers.TabIndex = 0;
+            // 
+            // lblCompanyName
+            // 
+            this.lblCompanyName.AutoSize = true;
+            this.lblCompanyName.Location = new System.Drawing.Point(27, 199);
+            this.lblCompanyName.Name = "lblCompanyName";
+            this.lblCompanyName.Size = new System.Drawing.Size(82, 13);
+            this.lblCompanyName.TabIndex = 1;
+            this.lblCompanyName.Text = "Company Name";
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Location = new System.Drawing.Point(71, 232);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(38, 13);
+            this.lblPhone.TabIndex = 2;
+            this.lblPhone.Text = "Phone";
+            // 
+            // txtCompanyName
+            // 
+            this.txtCompanyName.Location = new System.Drawing.Point(115, 199);
+            this.txtCompanyName.Name = "txtCompanyName";
+            this.txtCompanyName.Size = new System.Drawing.Size(201, 20);
+            this.txtCompanyName.TabIndex = 3;
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Location = new System.Drawing.Point(115, 232);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(126, 20);
+            this.txtPhone.TabIndex = 4;
+            // 
+            // btInsert
+            // 
+            this.btInsert.Location = new System.Drawing.Point(43, 283);
+            this.btInsert.Name = "btInsert";
+            this.btInsert.Size = new System.Drawing.Size(75, 23);
+            this.btInsert.TabIndex = 5;
+            this.btInsert.Text = "Insert";
+            this.btInsert.UseVisualStyleBackColor = true;
+            // 
+            // btUpdate
+            // 
+            this.btUpdate.Location = new System.Drawing.Point(124, 283);
+            this.btUpdate.Name = "btUpdate";
+            this.btUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btUpdate.TabIndex = 6;
+            this.btUpdate.Text = "Update";
+            this.btUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btDelete
+            // 
+            this.btDelete.Location = new System.Drawing.Point(205, 283);
+            this.btDelete.Name = "btDelete";
+            this.btDelete.Size = new System.Drawing.Size(75, 23);
+            this.btDelete.TabIndex = 7;
+            this.btDelete.Text = "Delete";
+            this.btDelete.UseVisualStyleBackColor = true;
+            // 
+            // FormShippers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 331);
+            this.Controls.Add(this.btDelete);
+            this.Controls.Add(this.btUpdate);
+            this.Controls.Add(this.btInsert);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.txtCompanyName);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblCompanyName);
+            this.Controls.Add(this.gridShippers);
+            this.Name = "FormShippers";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Shippers";
+            ((System.ComponentModel.ISupportInitialize)(this.gridShippers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView gridShippers;
+        private System.Windows.Forms.Label lblCompanyName;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.TextBox txtCompanyName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Button btInsert;
+        private System.Windows.Forms.Button btUpdate;
+        private System.Windows.Forms.Button btDelete;
+    }
+}
+
